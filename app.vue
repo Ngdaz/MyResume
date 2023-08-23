fcb21075c44b8fdab3aa5ab36e46330e3e4aecff
ngda <ngdaaz@gmail.com>
2023-08-23 17:16:54 +0700

    add config

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <body>
+    <aside>
+      <header></header>
+    </aside>
+    <div><HomePage /></div>
+  </body>
+</template>
+<script setup lang="ts">
+import HomePage from '~/components/HomePage/HomePage.vue'
+</script>
