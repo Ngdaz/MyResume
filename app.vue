7da92d92476ab786377ba93c6af7633438bb48cc
ngda <ngdaaz@gmail.com>
2023-09-05 17:39:42 +0700

    Aside

--- app.vue
+++ app.vue
@@ -1,13 +1,31 @@
 <template>
   <body>
-    <aside>
-      <header>
-
-      </header>
-    </aside>
-    <div>1</div>
+    <div class="grid grid-cols-12 gap-1">
+      <aside class="col-span-2 h-screen sticky top-0 self-start">
+        <div class="flex justify-center w-full">
+          <p>logo</p>
+        </div>
+        <div class="h-full flex item-center justify-center">
+          <ul class="header-menu flex flex-col justify-center">
+            <li
+              v-for="item in config.sectionName"
+              key="item"
+              class="mb-3 flex items-center"
+            >
+              <div class="w-[20px] h-[11px] rounded-[5.5px] bg-[#FF6B00]"></div>
+              <a :class="{ item }" class="block text-left ms-2">{{ item }}</a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+      <main class="bg-[#fff3ea] col-span-10">
+        <h1 class="text-center"></h1>
+        <h2 class="text-center"></h2>
+      </main>
+    </div>
   </body>
 </template>
 <script setup lang="ts">
 import HomePage from '~/components/HomePage/HomePage.vue'
+import { config } from './config.js'
 </script>
---
+++ config.js
@@ -0,0 +1,12 @@
+export const config = {
+  sectionName: [
+    'Home',
+    'About',
+    'Services',
+    'Resume',
+    'Recent Work',
+    'Testimonial',
+    'Blog',
+  ],
+}
+
